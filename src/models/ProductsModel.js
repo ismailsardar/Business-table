7ed0315f764fa36b79e3d7335dbb002fa7fa9e8f
Ismile Sardar <98180464+ismailsardar@users.noreply.github.com>
2023-04-08 15:03:09 +0600

    product model add

--- src/models/ProductsModel.js
+++ src/models/ProductsModel.js
@@ -1,22 +1,15 @@
 const mongoose = require("mongoose");
-const DataSchema = mongoose.Schema(
+const DataSchema = new mongoose.Schema(
   {
-    id: { type: Number },
-    title: { type: String },
-    price: { type: String },
-    special_price: { type: String },
-    image: { type: String },
-    category: { type: String },
-    subcategory: { type: String },
-    remark: { type: String },
+    name: { type: String },
     brand: { type: String },
-    shop: { type: String },
-    shop_name: { type: String },
-    star: { type: String },
-    product_code: { type: String },
-    stock: { type: String },
+    price: { type: String },
+    image_link: { type: String },
+    description: { type: String },
+    category: { type: String },
   },
   { versionKey: false }
 );
-const ProductsModel = mongoose.model("products", DataSchema);
-module.exports = ProductsModel;
+
+const DameJson = mongoose.model("demoJSON", DataSchema);
+module.exports = DameJson;
