95c2f17439ca9346e0eaad6d61e262ab0a018487
Ismile Sardar <98180464+ismailsardar@users.noreply.github.com>
2023-04-08 15:03:48 +0600

    product List controller function add

--- src/controllers/ProductsController.js
+++ src/controllers/ProductsController.js
@@ -1,35 +1,52 @@
-const ProductsModel = require("../models/ProductsModel");
+const { default: axios } = require("axios");
+const DameJson = require("../models/ProductsModel");
+const BaseUrl = "https://makeup-api.herokuapp.com/api/v1/products.json";
+
+exports.employList = async (req, res) => {
+  try {
+    // let data = await DameJson.find({});
+    let { data } = await axios.get(BaseUrl);
+    console.log(data.length);
+    // await data.map((item) =>
+    //   DameJson.create({
+    //     name: item.name,
+    //     brand: item.brand,
+    //     price: item.price,
+    //     image_link: item.image_link,
+    //     description: item.description,
+    //     category: item.category,
+    //   })
+    // );
+    res.status(200).json({ status: "success", data: data });
+  } catch (error) {
+    res.status(400).json({ status: "fail", error: error.message });
+  }
+};
 
 exports.productList = async (req, res) => {
   try {
-    let pageNo = Number(req.params.pageNo);
-    let perPage = Number(req.params.perPage);
-    let searchValue = req.params.searchKey;
+    const pageNo = Number(req.params.pageNo);
+    const perPage = Number(req.params.perPage);
+    const searchValue = req.params.searchKey;
     const skipRow = (pageNo - 1) * perPage;
-    let Total;
     let Rows;
-    if (searchValue !== "0") {
-      let searchRgx = { $regex: searchValue, $options: "i" };
-      let searchQuery = { $or: [{ title: searchRgx }] };
-      Total = await ProductsModel.aggregate([
-        { $match: searchQuery },
-        { $count: "total" },
-      ]);
-      Rows = await ProductsModel.aggregate([
-        { $match: searchQuery },
-        { $skip: skipRow },
-        { $limit: perPage },
-      ]);
-    } else {
-      Total = await ProductsModel.aggregate([{ $count: "total" }]);
-      Rows = await ProductsModel.aggregate([
-        { $skip: skipRow },
-        { $limit: perPage },
-      ]);
-    }
-
+    let Total;
+  
+    const searchQuery = searchValue !== "0" ?
+      { $or: [{ name: { $regex: searchValue, $options: "i" } }] } :
+      {};
+  
+    Total = (await DameJson.aggregate([{ $match: searchQuery }, { $count: "total" }]))[0]?.total || 0;
+  
+    Rows = await DameJson.aggregate([
+      { $match: searchQuery },
+      { $skip: skipRow },
+      { $limit: perPage }
+    ]);
+  
     res.status(200).json({ status: "success", total: Total, data: Rows });
+  
   } catch (error) {
-    res.status(400).json({ status: "fail", error: error.message });
+    res.status(500).json({status: "fail",error: error.message });
   }
 };
