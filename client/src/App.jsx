9e9726de8d56ed307bded6c1dea7da03ca86cf23
Ismile Sardar <98180464+ismailsardar@users.noreply.github.com>
2023-04-08 20:54:59 +0600

    app.js file update

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,33 +1,12 @@
-import { useState } from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
-import CanceledPage from "./pages/CanceledPage";
-import CompletedPage from "./pages/CompletedPage";
-import CreatePage from "./pages/CreatePage";
-import DashboardPage from "./pages/DashboardPage";
-import LoginPage from "./pages/LoginPage";
-import NewTaskPage from "./pages/NewTaskPage";
-import Page404 from "./pages/Page404";
-import ProfilePage from "./pages/ProfilePage";
-import ProgressPage from "./pages/ProgressPage";
-import RegistrationPage from "./pages/RegistrationPage";
+import ProductList from "./pages/ProductList";
 
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
     <>
       <BrowserRouter>
         <Routes>
-          <Route path="/" element={<DashboardPage />} />
-          <Route path="/create" element={<CreatePage />} />
-          <Route path="/all" element={<NewTaskPage />} />
-          <Route path="/progress" element={<ProgressPage />} />
-          <Route path="/registration" element={<RegistrationPage />} />
-          <Route path="/login" element={<LoginPage />} />
-          <Route path="/completed" element={<CompletedPage />} />
-          <Route path="/canceled" element={<CanceledPage />} />
-          <Route path="/profile" element={<ProfilePage />} />
-          <Route path="*" element={<Page404 />} />
+          <Route path="/" element={<ProductList />} />
         </Routes>
       </BrowserRouter>
     </>
